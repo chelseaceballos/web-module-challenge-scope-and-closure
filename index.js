3730c2344e51f01886b0fd0246ed0aac50e009f4
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-02-22 11:29:26 -0800

    completed task 1

--- index.js
+++ index.js
@@ -28,11 +28,15 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+  counter1 uses closures and counter2 is written in a shorter way without closures
+
   2. Which of the two uses a closure? How can you tell?
+  counter1 uses a closure starts on line 41
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+     counter1 code is preferable in . counter2 code is preferable in .
+     
 */
 
 // counter1 code
